b47bed13835027047b03e9eb2660a4a294527996
Pardeep <pardeepkumar63281@gmail.com>
2023-04-04 19:11:26 +0530

    Intial Commit

---
+++ LginANDRegister_TASK/Backend_Login_Register_Task/Backend_Login_Register_Task/Repository/EncryptionRepository.cs
@@ -0,0 +1,34 @@
+﻿using Backend_Login_Register_Task.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Login_Register_Task.Repository
+{
+    public class EncryptionRepository : IEncryptionRepository
+    {
+       
+
+        public string EncryptPassword(string password)
+        {
+            byte[] encData_byte = new byte[password.Length];
+            encData_byte = System.Text.Encoding.UTF8.GetBytes(password);
+            string encodedData = Convert.ToBase64String(encData_byte);
+            return encodedData;
+        }
+        public string DecryptPassword(string password)
+        {
+            string decryptpwd = string.Empty;
+            UTF8Encoding encodepwd = new UTF8Encoding();
+            Decoder Decode = encodepwd.GetDecoder();
+            byte[] todecode_byte = Convert.FromBase64String(password);
+            int charCount = Decode.GetCharCount(todecode_byte, 0, todecode_byte.Length);
+            char[] decoded_char = new char[charCount];
+            Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
+            decryptpwd = new String(decoded_char);
+            return decryptpwd;
+        }
+    }
+}
---
+++ LginANDRegister_TASK/Backend_Login_Register_Task/Backend_Login_Register_Task/SD.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace Backend_Login_Register_Task
+{
+    public static class SD
+    {
+        public static string Encrypt(string username, byte[] key, byte[] iv)
+        {
+            // Create an Aes object
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+
+                // Create an encryptor to perform the stream transform
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                // Create the streams used for encryption
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            // Write the plaintext to the stream
+                            swEncrypt.Write(username);
+                        }
+
+                        // Return the encrypted bytes from the memory stream
+                        return Convert.ToBase64String(msEncrypt.ToArray());
+                    }
+                }
+            }
+        }
+    
+public static string Decrypt(string cipherText, byte[] key, byte[] iv)
+{
+    // Create an Aes object
+    using (Aes aes = Aes.Create())
+    {
+        aes.Key = key;
+        aes.IV = iv;
+
+        // Create a decryptor to perform the stream transform
+        ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+        // Create the streams used for decryption
+        using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+        {
+            using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+            {
+                using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                {
+                    // Read the decrypted bytes from the decrypting stream
+                    return srDecrypt.ReadToEnd();
+                }
+            }
+        }
+    }
+}
+    }
+}
